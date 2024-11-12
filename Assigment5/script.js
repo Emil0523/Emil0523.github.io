9f39bb649047bdd71d48a9214f52779c7e437015
Emil Amundsen <119764056+Emil0523@users.noreply.github.com>
2024-11-12 15:54:51 +0100

    asdasdasdasdadasdad

--- Assigment5/script.js
+++ Assigment5/script.js
@@ -1,4 +1,9 @@
 function showPopup(event) {
+    const openPopups = document.querySelectorAll('.popup.show');
+    openPopups.forEach(popup => {
+        hidePopup(popup.id);
+    });
+
     const popupId = event.currentTarget.getAttribute('data-popup');
     const popup = document.getElementById(popupId);
     const rect = event.currentTarget.getBoundingClientRect();
@@ -18,5 +23,34 @@ function hidePopup(popupId) {
     popup.classList.add('hide');
     setTimeout(() => {
         popup.style.display = "none";
-    }, 300); // Matcha med animationens längd
+    }, 300);
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
